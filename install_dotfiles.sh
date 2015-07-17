edbec7799240296be1f6a44de65e0f85872c670b
Erik Derohanian <erik@lyricalsecurity.com>
2015-07-17 19:50:35 -0400

    add install script

---
+++ install_dotfiles.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+  ##################################
+  #     Create symlinks to all     #
+  # dotfiles in the home directory #
+  ##################################
+
+# Variables
+
+dir=~/configs                           # dotfiles directory
+olddir=~/configs_old                   # old dotfiles backup directory
+files="bashrc vimrc toprc gitconfig"    # list of files/folders to symlink in homedir
+
+##########
+
+# create configs_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
