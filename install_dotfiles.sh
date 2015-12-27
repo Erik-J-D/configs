3365f42899d474f23661b692356d78d9a13e49f0
Erik Derohanian <erik@erik-j.de>
2015-12-27 15:07:39 -0500

    update to more recent fork of ctrlp

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -34,4 +34,4 @@ done
 # get pathogen and vim plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
-git clone https://github.com/kien/ctrlp.vim ~/.vim/bundle/ctrlp
+git clone https://github.com/ctrlpvim/ctrlp.vim ~/.vim/bundle/ctrlp
